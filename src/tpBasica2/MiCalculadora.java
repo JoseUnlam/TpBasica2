6da59645a72db4982197ef2d16c8b7e75ae0c247
LeandroBu <leandro.ariel.gomez1@gmail.com>
2019-09-22 00:10:52 -0300

    Update MiCalculadora

--- src/tpBasica2/MiCalculadora.java
+++ src/tpBasica2/MiCalculadora.java
@@ -14,26 +14,52 @@ public class MiCalculadora {
 		Scanner teclado = new Scanner(System.in);
 		
 		do{
-			System.out.println("Ingrese su operación: 1 para sumar, 2 para restar, 3 para multiplicar, 4 para dividir");
-			System.out.println("Luego, ingrese los numeros que desee operar");
-			opcion = teclado.nextInt();	
+			System.out.print("Ingrese su operacion: 1 para multiplicar , 2 para dividir, 3 para sumar, 4 para restar: ");
+			
+			opcion = teclado.nextInt();
+			
+			System.out.println("\nLuego, ingrese los numeros que desee operar");
 		}while(opcion <= 1 && opcion >= 4);
 		
 			switch(opcion) {
 				case 1:
+					System.out.print("Ingrese el primero numero: ");
+					a = teclado.nextInt();
+					System.out.print("Ingrese el segundo numero: ");
+					b = teclado.nextInt();
 					
+					Integer multiplicar = a*b;
+					System.out.println("\nEl resultado es: "+multiplicar);
 				break;
 				
 				case 2:
+					System.out.print("Ingrese el primero numero: ");
+					a = teclado.nextInt();
+					System.out.print("Ingrese el segundo numero: ");
+					b = teclado.nextInt();
 					
+					Integer dividir = a/b;
+					System.out.println("\nEl resultado es: "+dividir);
 				break;
 				
 				case 3:
+					System.out.print("Ingrese el primero numero: ");
+					a = teclado.nextInt();
+					System.out.print("Ingrese el segundo numero: ");
+					b = teclado.nextInt();
 					
+					Integer sumar = a+b;
+					System.out.println("\nEl resultado es: "+sumar);
 				break;
 				
 				case 4:
+					System.out.print("Ingrese el primero numero: ");
+					a = teclado.nextInt();
+					System.out.print("Ingrese el segundo numero: ");
+					b = teclado.nextInt();
 					
+					Integer restar = a-b;
+					System.out.println("\nEl resultado es "+restar);
 				break;
 			}
 	}
