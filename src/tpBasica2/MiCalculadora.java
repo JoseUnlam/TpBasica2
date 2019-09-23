0abe54b1d0453fd5f12bd2cb4028da37b8f018a9
JoseUnlam <h_joseunlam@hotmail.com>
2019-09-23 19:55:32 -0300

    Completando las respuestas

--- src/tpBasica2/MiCalculadora.java
+++ src/tpBasica2/MiCalculadora.java
@@ -1,61 +1,17 @@
 package tpBasica2;
 
 public class MiCalculadora {
-	public static void main(String[] args) {
-		
-		//atributos
+	
 		Integer a = 0;
 		Integer b = 0;
 		Integer opcion;
 		Integer resultado;
 		
-		Scanner teclado = new Scanner(System.in);
-		
-		do{
-			System.out.println("Ingrese su operaciÃ³n: 1 para sumar, 2 para restar, 3 para multiplicar, 4 para dividir");
-			System.out.println("Luego, ingrese los numeros que desee operar");
-			opcion = teclado.nextInt();	
-		}while(opcion <= 1 && opcion >= 4);
-		
-			switch(opcion) {
-				case 1:
-					System.out.println("Ingrese el primer numero");
-					 a = teclado.nextInt();
-					 System.out.println("Ingrese el segundo numero");
-					 b = teclado.nextInt();
-				break;
+			public MiCalculadora(Integer a, Integer b) {
 				
-				case 2:
-					System.out.println("Ingrese el primer numero");
-					 a = teclado.nextInt();
-					 System.out.println("Ingrese el segundo numero");
-					 b = teclado.nextInt();
-					
-				break;
-				
-				case 3:
-					System.out.println("Ingrese el primer numero");
-					 a = teclado.nextInt();
-					 System.out.println("Ingrese el segundo numero");
-					 b = teclado.nextInt();
-					
-				break;
-				
-				case 4:
-					System.out.println("Ingrese el primer numero");
-					 a = teclado.nextInt();
-					 System.out.println("Ingrese el segundo numero");
-					 b = teclado.nextInt();
-					
-				break;
-			
+				this.a = a;
+				this.b = b;
 			}
-	
-	public MiCalculadora(Integer a, Integer b) {
-	
-		this.a = a;
-		this.b = b;
-	}
 	
 	public int  sumar() {
 		return this.a + this.b;
@@ -79,16 +35,35 @@ public class MiCalculadora {
 			return this.b / this.a;
 		}
 		}
-}		
-		
-//Â¿QuÃ© es GIT FLOW?
-//Git-flow es un conjunto de extensiones para Git, agrega comandos que por detras
-//usan los comandos de git.
+	}
+			
+//¿Qué sucedió cuando se intento hacer el segundo push?
+// Se genero conflictos con la rama master
+//¿Qué acciones hay que tomar antes de realizar el push?
+//Hacer un pull de la rama master y comparar las modificaciones recientes
+//¿Qué estrategia propondrían para evitar esta situación?
+//Comentar las modificaciones y trabajar en una rama aparte
+//¿Qué acción realiza el parámetro -b?
 
-//Â¿CÃ³mo puede ayudarnos a trabajar en equipo?
-// ayuda a trabajar dando la posibilidad de trabajar sobre copias de los contenidos de Develop y Master, las
-//cuales son develop y hotfix.
+//Realizar en el repositorio remoto un merge request
+//
+//¿Qué diferencias notaron en el proceso?
+//se abrio un merge luego del pull -Pull request successfully merged and closed- y permite comparar cambios entre
+//los distintos branches y commits, e incluso entre forks
+//¿Qué estrategia utilizarían para evitar conflictos?
+//leer los cambios a medida que se van aplicando y comentar en cada caso que fue lo que se introdujo
+//que mejoras o correcciones hubo, comentandolo en cada merge request
 
-//Â¿Se relaciona con alguna de las estrategias planteadas? Â¿Como?
-//Esto logra que al trabajar en equipo, mientras correjimos algo en el codigo del programa, podemos hacerlo sin
-//alterar el trabajo de otro miembro del equipo.		
+
+//¿Qué es GIT FLOW?
+//Git-flow es un conjunto de extensiones para Git, agrega comandos que por detras usan los comandos de git.
+
+//¿Cómo puede ayudarnos a trabajar en equipo?
+//ayuda a trabajar dando la posibilidad de trabajar sobre copias de los contenidos de Develop y Master, las cuales son develop y hotfix.
+
+//¿Se relaciona con alguna de las estrategias planteadas? ¿Como?
+//Esto logra que al trabajar en equipo, mientras correjimos algo en el codigo del programa, podemos hacerlo sin alterar el trabajo de otro miembro del equipo.
+
+
+
+
--- src/tpBasica2/MiCalculadoraMain.java
+++ src/tpBasica2/MiCalculadoraMain.java
@@ -51,6 +51,6 @@ public class MiCalculadoraMain {
 							System.out.println("Resultado: "+resultado);
 						break;
 					}
-	}
-
+	}	
+	
 }
